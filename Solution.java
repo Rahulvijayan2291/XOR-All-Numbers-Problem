906827fd4cc482b1700e730efc7ebd7fb8cca6da
Rahul Vijayan <45307220+Rahulvijayan2291@users.noreply.github.com>
2025-01-16 08:40:22 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int xorNums1 = 0;
+        int xorNums2 = 0;
+
+        for (int num : nums1) {
+            xorNums1 ^= num;
+        }
+
+        for (int num : nums2) {
+            xorNums2 ^= num;
+        }
+
+        int result = 0;
+        if (nums1.length % 2 != 0) {
+            result ^= xorNums2;
+        }
+        if (nums2.length % 2 != 0) {
+            result ^= xorNums1;
+        }
+
+        return result;
+    }
+}
